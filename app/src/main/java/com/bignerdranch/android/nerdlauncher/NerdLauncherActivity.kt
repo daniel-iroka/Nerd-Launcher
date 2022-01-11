6b589b38904dec1e5f12615e6393ddd0e55b3531
daniel-iroka <danieliroka67@gmail.com>
2022-01-11 22:27:02 +0100

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.kt
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.nerdlauncher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NerdLauncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nerd_launcher)
+
+        // TODO : Add RecyclerView dependency to this Project and then hook it up to our Activity.....
+    }
+}
