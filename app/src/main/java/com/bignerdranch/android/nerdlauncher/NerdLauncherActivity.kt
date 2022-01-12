77ff44743613841239454d2d5b9022320c1d9693
daniel-iroka <danieliroka67@gmail.com>
2022-01-12 18:02:47 +0100

    Added our NerdLauncher Activity and RecyclerView

--- app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.kt
+++ app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.kt
@@ -1,13 +1,36 @@
 package com.bignerdranch.android.nerdlauncher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
+private const val TAG = "NerdLauncherActivity"
+
+// TODO : When I come back, start from "Querying the PackageManager"
 class NerdLauncherActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nerd_launcher)
 
-        // TODO : Add RecyclerView dependency to this Project and then hook it up to our Activity.....
+        recyclerView = findViewById(R.id.app_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)  // this puts our LinearLayout in a vertical format
+
+        setUpAdapter()
+    }
+
+    private fun setUpAdapter() {
+        val startupIntent = Intent(Intent.ACTION_MAIN).apply {
+            addCategory(Intent.CATEGORY_LAUNCHER)
+        }
+
+        val activities = packageManager.queryIntentActivities(startupIntent, 0)
+
+        Log.i(TAG, "Found ${activities.size} activities.")
     }
 }
